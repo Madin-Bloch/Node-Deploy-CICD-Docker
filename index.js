03ea658f62424d3eb9212823b59820133292955b
Madin-Bloch <shineinfosoft109@gmail.com>
2026-02-13 16:17:21 +0530

    Add Dockerfile, update index.js for PORT handling, and add start script to package.json

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.get("/", (req, res) => {
   });
 });
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
